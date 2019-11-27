c6df4cf908cebb519940d9e73a77b78f11b08464
Anton <mokhovikov.biz@yandex.ru>
2019-11-27 17:55:44 +0300

    init

---
+++ wp-content/cache/wpfc-minified/1v40kks/dgrx5.js
@@ -0,0 +1,7 @@
+// source --> https://fredrikvirtanen.se/wp-content/plugins/flow-flow-social-streams/js/require-utils.js?ver=3.1.36 
+var FF_resource = {
+	scriptDeferred: jQuery.Deferred(),
+	styleDeferred: jQuery.Deferred(),
+	scriptLoading: false,
+	styleLoading: false
+};
